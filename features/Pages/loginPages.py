c014505b25db040d05c1afd95e443a244ffebbc8
Ashwini Deepak <ashwini.ashu123@gmail.com>
2026-04-01 12:17:54 +0530

    changes 2

--- features/Pages/loginPages.py
+++ features/Pages/loginPages.py
@@ -18,4 +18,5 @@ class loginPages:
     async def verifyLogin(self):
         await self.page.get_by_title("All Warehouses").wait_for(timeout=10000)
         print("Login successful")
+        await self.page.wait_for_timeout(5000)
         await self.page.context.storage_state(path="state.json")
